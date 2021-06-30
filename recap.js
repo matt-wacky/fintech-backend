a0153aaaac237a68858a4d6aac2e86d7db35d754
matt-wacky <85351723+matt-wacky@users.noreply.github.com>
2021-06-30 17:13:02 +0800

    Add files via upload

---
+++ recap.js
@@ -0,0 +1,30 @@
+var x = 20
+var y = 50
+
+console.log(x)
+console.log(y)
+console.log ("matt")
+
+if (y > 18) {
+    console.log("you are an adult");}
+    else {
+    console.log("you are a kid");
+}
+
+function add(x,y){
+    x = y*3
+}
+
+add (2,3)
+console.log(x)
+
+let x2 = Boolean
+let x3 = Boolean
+
+x2=0;
+
+if (x2 = true) {
+    console.log("Start the pump");}
+    else {
+    console.log("Close Valve HCV5852-X");
+    }
